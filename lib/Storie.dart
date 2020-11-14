3013dcf6e063a781f3df141ecbddfa9b85a7ce8f
Moha_ <ahrir@live.com>
2020-11-14 03:47:33 +0100

    stories D, posts 22, 42, 51

--- lib/Storie.dart
+++ lib/Storie.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Storie {
+  //para listas de stories!
   String name;
   String image;
-
   Storie({this.name, this.image});
 }
--- lib/home.dart
+++ lib/home.dart
@@ -6,16 +6,107 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white70,
+        backgroundColor: Colors.white,
         appBar: _appBar(),
-    body: Column(
-    children: <Widget>[_etiquetas(), _historias()],
-    )
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              _etiquetas(),
+              _historias(),
+              Divider(
+                color: Colors.red,
+                height: 0,
+                thickness: 0.6,
+              ),
+              _post()
+            ],
+          ),
+        ));
+  }
+
+  Widget _post() {
+    return Container(
+      width: double.infinity,
+      height: 400,
+      // listbuilder para la memoria, mas eficiente cuando haya muchos datos
+      child: ListView.builder(
+        itemCount: 30,
+        itemBuilder: (context, i) {
+          return _creatPost("assets/img/str_1.jpg", "assets/img/post_1.jpg");
+        },
+      ),
     );
   }
 
-  Widget _navBar() {
-    return Container();
+  Widget _creatPost(String asset, String imgPost) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 10, left: 5, bottom: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image(
+                      image: AssetImage(asset),
+                      width: 60,
+                      height: 60,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Text(
+                  "Moha_",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                Expanded(child: SizedBox()),
+                IconButton(icon: Icon(Icons.more_horiz), onPressed: () {})
+              ],
+            ),
+          ),
+          FadeInImage(
+            placeholder: AssetImage(imgPost),
+            image: AssetImage(imgPost),
+          ),
+          Container(
+            padding: EdgeInsets.all(3.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(FontAwesome.heart_o), onPressed: () {}),
+                    IconButton(
+                        icon: Icon(FontAwesome.comment_o), onPressed: () {}),
+                    IconButton(
+                        icon: Icon(FontAwesome.send_o), onPressed: () {}),
+                  ],
+                ),
+                IconButton(icon: Icon(FontAwesome.bookmark_o), onPressed: () {})
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 5),
+            child: Row(
+              children: <Widget>[Text("Like by Mksd22, Aeoor, 10 others")],
+            ),
+          ),
+          /*
+          Container(
+            child: Column(
+              children: <Widget> [
+
+              ],
+            ),
+          )
+           */
+        ],
+      ),
+    );
   }
 
   Widget _appBar() {
@@ -134,9 +225,9 @@ class HomePage extends StatelessWidget {
           user,
           style: user == ("Your story")
               ? TextStyle(
-              fontSize: 14,
-              color: Colors.black87,
-              fontWeight: FontWeight.bold)
+                  fontSize: 14,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold)
               : TextStyle(fontSize: 14, color: Colors.black87),
         ),
       ],
